e6e8d6a4d270c6c55a751972708b158939d7b01f
Eliakim <103770049+techLii@users.noreply.github.com>
2025-09-09 19:27:34 +0300

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="/rss"
+            href="https://linkedin.com/techlii"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">linkedin</p>
