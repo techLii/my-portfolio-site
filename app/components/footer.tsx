205da3402c33182c9d6d26d8c0d731043e989023
Eliakim <103770049+techLii@users.noreply.github.com>
2025-09-09 19:53:23 +0300

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -43,7 +43,7 @@ export default function Footer() {
         </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
+        © {new Date().getFullYear()} ELiakim Nyawinda
       </p>
     </footer>
   )
