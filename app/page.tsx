903c7d1879e530e205429a9c0e86e2c3bedb5267
techLii <eleeakim@gmail.com>
2025-11-07 05:27:21 +0300

    Added a profile picture

--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,34 @@ import { BlogPosts } from 'app/components/posts'
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        
-      </h1>
+      <div className="flex items-center mb-8">
+        <img 
+          src="images/profile-picture.png" 
+          alt="Eli's Profile" 
+          className="w-20 h-20 rounded-full mr-4 object-cover" 
+        />
+        <h1 className="text-2xl font-semibold tracking-tighter">
+          Eliakim Nyawinda
+        </h1>
+      </div>
       <p className="mb-4">
-        {`Hi I'm Eli, I am a technical trainer, when I'm not teaching I'm coding.`}
+        {`Hi I'm Eli, a technical trainer based in Eldoret, Kenya.`}
       </p>
       
       <div className="my-8">
-        <h2 className="text-xl font-semibold mb-4">My Projects</h2>
+        <h2 className="text-xl font-semibold mb-4">Check out some of my work</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="border rounded p-4">
             <h3 className="text-lg font-medium mb-2"> <a href="https://www.scrideconsulting.agency" target="_blank" rel="noopener noreferrer">
-              Scride Consulting</a></h3>
-            <img src="/projects/scride-consulting.jpg" alt="Scride Consulting" className="w-full h-auto mb-2 rounded" />
-            <p className="text-sm"><a href="https://www.scrideconsulting.agency" target="_blank" rel="noopener noreferrer">A consulting platform focused on technical solutions and advisory services.</a></p>
+              Scride Consulting Agency</a></h3>
+            <img src="/images/scride-logo.png" alt="Scride Consulting" className="w-full h-auto mb-2 rounded" />
+            <p className="text-sm"><a href="https://www.scrideconsulting.agency" target="_blank" rel="noopener noreferrer">Scride helps organizations improve their operations through strategic analysis, program management, and sustainability consulting.</a></p>
           </div>
           <div className="border rounded p-4">
             <h3 className="text-lg font-medium mb-2"> <a href="https://www.thrivefromthorns.org" target="_blank" rel="noopener noreferrer">
-              Thrive From Thorns</a></h3>
-            <img src="/projects/scride-consulting.jpg" alt="Thrive From Thorns" className="w-full h-auto mb-2 rounded" />
-            <p className="text-sm"><a href="https://www.thrivefromthorns.org" target="_blank" rel="noopener noreferrer">A consulting platform focused on technical solutions and advisory services.</a></p>
+              Thrive From Thorns Kenya</a></h3>
+            <img src="/images/tft-logo.png" alt="Thrive From Thorns" className="w-full h-auto mb-2 rounded" />
+            <p className="text-sm"><a href="https://www.thrivefromthorns.org" target="_blank" rel="noopener noreferrer">A nonprofit tackling root causes of poverty and trauma to help communities in Kenya thrive.</a></p>
           </div>
         </div>
       </div>
